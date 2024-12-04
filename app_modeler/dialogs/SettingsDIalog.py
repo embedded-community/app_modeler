9b486e9093b43d76eb9a5231b2183800d56895a9
Jussi Vatjus-Anttila <jussiva@gmail.com>
2024-12-04 18:42:46 +0200

    possibility to open config file folder

--- app_modeler/dialogs/SettingsDIalog.py
+++ app_modeler/dialogs/SettingsDIalog.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+import logging
+
+from PySide6.QtGui import QDesktopServices
 from PySide6.QtWidgets import QDialog, QVBoxLayout, QPushButton
-from PySide6.QtCore import QSettings
+from PySide6.QtCore import QSettings, QUrl
 
 from app_modeler.models.AppSettings import AppSettings
 from app_modeler.widgets.FormGenerator import FormGenerator
 from app_modeler.widgets.SettingsWidget import SettingsWidget
+
+logger = logging.getLogger(__name__)
 
 
 class AppSettingsWidget(SettingsWidget):
@@ -30,11 +36,25 @@ class SettingsDialog(QDialog):
         layout = QVBoxLayout()
         self.settings_widget = AppSettingsWidget(self.settings, self.app_settings)
         layout.addWidget(self.settings_widget)
+
+        app_config_folder_button = QPushButton("Open application config folder")
+        app_config_folder_button.clicked.connect(self.on_open_config_folder)
+        layout.addWidget(app_config_folder_button)
         # accept button
         self.accept_button = QPushButton("Close")
         layout.addWidget(self.accept_button)
         self.accept_button.clicked.connect(self.accept)
         self.setLayout(layout)
+
+    def on_open_config_folder(self):
+        absolute_path = Path(self.settings.fileName()).resolve().parent
+        # open folder to OS file browser
+        # Convert the path to a file URL
+        url = QUrl.fromLocalFile(absolute_path)
+
+        # Use QDesktopServices to open the folder
+        if not QDesktopServices.openUrl(url):
+            logger.warning(f"Failed to open folder: {absolute_path}")
 
 if __name__ == '__main__':
     from PySide6.QtWidgets import QApplication

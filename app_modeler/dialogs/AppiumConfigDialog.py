6a2059c59e66069acf287860d1ade5c3b509d617
Jussi Vatjus-Anttila <jussiva@gmail.com>
2024-12-05 20:03:47 +0200

    option to import and export appium configurations (#5)

--- app_modeler/dialogs/AppiumConfigDialog.py
+++ app_modeler/dialogs/AppiumConfigDialog.py
@@ -1,4 +1,6 @@
-from PySide6.QtWidgets import QDialog, QVBoxLayout, QPushButton
+import json
+
+from PySide6.QtWidgets import QDialog, QVBoxLayout, QPushButton, QGroupBox, QHBoxLayout, QFileDialog
 
 from app_modeler.widgets.AppiumOptionsWidget import AppiumOptionsWidget
 
@@ -12,11 +14,53 @@ class AppiumConfigDialog(QDialog):
         layout.addWidget(self.appium_options_widget)
         self.setLayout(layout)
 
+        mport_groupbox = QGroupBox("Import/Export")
+        layout.addWidget(mport_groupbox)
+        mpoert_layout = QHBoxLayout()
+        mport_groupbox.setLayout(mpoert_layout)
+        self.export_button = QPushButton("Export")
+        self.import_button = QPushButton("Import")
+        mpoert_layout.addWidget(self.export_button)
+        mpoert_layout.addWidget(self.import_button)
+
         # accept button
         self.accept_button = QPushButton("Close")
         layout.addWidget(self.accept_button)
+        self._connect_signals()
+
+    def _connect_signals(self):
         self.accept_button.clicked.connect(self.accept)
+        self.export_button.clicked.connect(self.on_export)
+        self.import_button.clicked.connect(self.on_import)
 
     @property
     def options(self):
         return self.appium_options_widget.options
+
+    def on_export(self):
+        data_to_save = self.appium_options_widget.to_dict()
+        # open file dialog for save json file
+        file_path, _ = QFileDialog.getSaveFileName(self, "Export JSON File", "", "JSON Files (*.json)")
+        if file_path:
+            with open(file_path, 'w') as file:
+                json.dump(data_to_save, file, indent=4)
+
+    def on_import(self):
+        # open file dialog for open json file
+        file_path, _ = QFileDialog.getOpenFileName(self, "Import JSON File", "", "JSON Files (*.json)")
+        if file_path:
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                self.appium_options_widget.from_dict(data)
+
+if __name__ == "__main__":
+    import sys
+    from PySide6.QtWidgets import QApplication
+    from PySide6.QtCore import QSettings
+
+    app = QApplication([])
+
+    dialog = AppiumConfigDialog(QSettings())
+    dialog.show()
+
+    sys.exit(app.exec_())
--- app_modeler/widgets/AppiumOptionsWidget.py
+++ app_modeler/widgets/AppiumOptionsWidget.py
@@ -45,11 +45,11 @@ class AppiumOptionsWidget(SettingsWidget):
         self.appium_server_line_edit.setValidator(QUrlValidator())
         self.appium_group_box_layout.addWidget(self.appium_server_line_edit)
 
-        self.options_combo = QComboBox()
-        self.options_combo.setObjectName('appium_driver_selector')
-        self.options_combo.addItems(self._appium_options.keys())
-        self.options_combo.currentTextChanged.connect(self.on_option_changed)
-        self.appium_group_box_layout.addWidget(self.options_combo)
+        self.driver_combo = QComboBox()
+        self.driver_combo.setObjectName('appium_driver_selector')
+        self.driver_combo.addItems(self._appium_options.keys())
+        self.driver_combo.currentTextChanged.connect(self.on_option_changed)
+        self.appium_group_box_layout.addWidget(self.driver_combo)
         layout.addWidget(self.appium_group_box)
 
         self.appium_options_box = QGroupBox("Appium Options")
@@ -59,7 +59,28 @@ class AppiumOptionsWidget(SettingsWidget):
 
         self.setLayout(layout)
 
-        self.on_option_changed(self.options_combo.currentText())
+        self.on_option_changed(self.driver_combo.currentText())
+
+    @property
+    def selected_driver(self):
+        return self.driver_combo.currentText()
+
+    @property
+    def appium_server(self):
+        return self.appium_server_line_edit.text()
+
+    def to_dict(self):
+        return {
+            'appium_server': self.appium_server,
+            'driver': self.selected_driver,
+            'capabilities': self.options.to_capabilities()
+        }
+
+    def from_dict(self, data):
+        self.appium_server_line_edit.setText(data['appium_server'])
+        self.driver_combo.setCurrentText(data['driver'])
+        self.on_option_changed(data['driver'])
+        self.options.load_capabilities(data['capabilities'])
 
     def on_option_changed(self, text):
         if self.form_generator:
